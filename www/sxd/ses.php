268d7d445616b10012a3813aea94e5dcd8e2bd3a
Sheezeequ <lubunter@601897.msk-kvm.ru>
2024-08-04 19:30:24 +0300

    update sxd

--- www/sxd/ses.php
+++ www/sxd/ses.php
@@ -1,31 +1,4 @@
 <?php
 $SES = array (
-  '8c68cbbeec77cf145c7e03ee617bb973' => 
-  array (
-    'cfg' => 
-    array (
-      'charsets' => 'cp1251 utf8 latin1',
-      'lang' => 'ru',
-      'time_web' => '600',
-      'time_cron' => '600',
-      'backup_path' => 'backup/',
-      'backup_url' => 'backup/',
-      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
-      'globstat' => 0,
-      'my_host' => 'localhost',
-      'my_port' => 3306,
-      'my_user' => 'ssaksa',
-      'my_pass' => 'sraksa1122',
-      'my_comp' => 0,
-      'my_db' => '',
-      'auth' => 'mysql cfg',
-      'user' => '',
-      'pass' => '',
-      'confirm' => '6',
-      'exitURL' => './',
-    ),
-    'time' => 1722151206,
-    'lng' => 'ru',
-  ),
 );
 ?>
